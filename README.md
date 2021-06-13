c0940140a9b4c95b7bfec5ff99eae455ec4c4d6f
Abdi Yusuf <abdiyuu7@gmail.com>
2021-06-13 13:27:37 -0600

    wired basic operations to buttons

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # calculator
+
+## TODO: 
+* Chaining of operations
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function add(...numbers) {
+function add(numbers) {
 	let sum = 0;
 	for(let num of numbers) {
 		sum+=num;
@@ -6,7 +6,7 @@ function add(...numbers) {
 	return sum;
 }
 
-function subtract(...numbers) {
+function subtract(numbers) {
 	let difference = numbers[0];
 	numbers.shift();
 	for(let num of numbers) {
@@ -15,7 +15,7 @@ function subtract(...numbers) {
 	return difference;
 }
 
-function multiply(...numbers) {
+function multiply(numbers) {
 	let product = numbers[0];
 	numbers.shift();
 	for(let num of numbers) {
@@ -24,7 +24,7 @@ function multiply(...numbers) {
 	return product;
 }
 
-function divide(...numbers) {
+function divide(numbers) {
 	let quotient = numbers[0];
 	numbers.shift();
 	for(let num of numbers) {
@@ -33,6 +33,57 @@ function divide(...numbers) {
 	return quotient;
 }
 
-function operate(operator, ... numbers) {
-	return operator(...numbers);
+function operate(operator, numbers) {
+	return operator(numbers);
 }
+
+function displayNum(e) {
+	let num = e.target.innerText;
+	const display = document.querySelector('h1');
+	display.innerText += num;
+
+}
+
+function clearDisplay() {
+	operands.push(Number(document.querySelector('h1').innerText))
+	document.querySelector('h1').innerText = '';
+}
+
+function storeOperation(e) {
+	clearDisplay();
+	operator = e.target.innerText;
+}
+
+function displayAnswer() {
+	clearDisplay();
+	switch(true) {
+		case (operator === '+'):
+			document.querySelector('h1').innerText = operate(add, operands);
+			break;
+		case (operator === '-'):
+			document.querySelector('h1').innerText = operate(subtract, operands);
+			break;
+		case (operator === 'x'):
+			document.querySelector('h1').innerText = operate(multiply, operands);
+			break;
+		case (operator === '/'):
+			document.querySelector('h1').innerText = operate(divide, operands);
+			break;
+		default:
+			document.querySelector('h1').innerText = 0;
+
+	}
+	operands = [];
+}
+
+const digits = document.querySelectorAll('.num');
+digits.forEach(digit => digit.addEventListener('click', displayNum));
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', storeOperation));
+
+const answer = document.querySelector('.answer');
+answer.addEventListener('click', displayAnswer);
+
+let operands = [];
+let operator;
